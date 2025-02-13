fb2e07235239a296201b651055efb3c1135b0f54
Jeff Ripke <jripke74@gmail.com>
2025-02-13 10:01:45 -0800

    add toggleMovieModal()

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,2 +1,8 @@
 const addMovieModal = document.getElementById("add-modal");
 const startAddMovieButton = document.querySelector("header button");
+
+const toggleMovieModel = () => {
+  addMovieModal.classList.toggle("visible");
+};
+
+startAddMovieButton.addEventListener("click", toggleMovieModel);
